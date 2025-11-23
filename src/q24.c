16f2149fa20b6995dd51a4572e41b7ab95ec001a
Angelina <angelinachaudhary89@gmail.com>
2025-11-23 14:24:30 +0545

    assignment completed

--- src/q24.c
+++ src/q24.c
@@ -1,5 +1,5 @@
 // Write a recursive function named calculatePower that takes two integers base and exponent as input and returns the result of raising the base to the exponent.
-#include <stdio.h>
+#include <stdio.h> git add .
 long long calculatePower(int base, int exponent) {
     if (exponent < 0) {
         printf("Negative exponents are not supported in this integer version.\n");
