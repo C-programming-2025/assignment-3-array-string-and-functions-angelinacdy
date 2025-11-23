06361d2fae9a0be5a2781072adb43666cc99ac7e
Angelina <angelinachaudhary89@gmail.com>
2025-11-23 13:26:21 +0545

    assignment completed

--- src/q9.c
+++ src/q9.c
@@ -1,1 +1,19 @@
 // Implement a C program to count the occurrence of a specific character in a string.
+#include <stdio.h>
+int main() {
+    char str[100];
+    char ch;
+    int count = 0;
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);  
+    printf("Enter the character to count: ");
+    scanf(" %c", &ch);    
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ch) {
+            count++;
+        }
+    }
+    printf("The character '%c' occurs %d time(s) in the string.\n", ch, count);
+    return 0;
+}
